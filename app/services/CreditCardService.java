3e42e3a0d93f138245cb7bed9e8cd241e40b6246
vita1ity <vitalii.oleksiv@gmail.com>
2016-05-17 20:11:47 +0300

    Added try-catch block for charge method

--- app/services/CreditCardService.java
+++ app/services/CreditCardService.java
@@ -28,6 +28,7 @@ import net.authorize.api.contract.v1.TransactionResponse.Messages;
 import net.authorize.api.contract.v1.TransactionTypeEnum;
 import net.authorize.api.controller.CreateTransactionController;
 import net.authorize.api.controller.base.ApiOperationBase;
+import play.Logger;
 
 
 @Singleton
@@ -35,42 +36,50 @@ public class CreditCardService {
 	
 	public ANetApiResponse charge(String apiLoginId, String transactionKey, Double amount, CreditCard userCreditCard) {
 
-
-        //Common code to set for all requests
-        //ApiOperationBase.setEnvironment(Environment.SANDBOX);
-		ApiOperationBase.setEnvironment(Environment.PRODUCTION);
-
-        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
-        merchantAuthenticationType.setName(apiLoginId);
-        merchantAuthenticationType.setTransactionKey(transactionKey);
-        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
-
-        // Populate the payment data
-        PaymentType paymentType = new PaymentType();
-        CreditCardType creditCard = new CreditCardType();
-        creditCard.setCardNumber(userCreditCard.digits);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
-        String expDateStr = userCreditCard.expDate.format(formatter);
-        creditCard.setExpirationDate(expDateStr);
-        creditCard.setCardCode(Integer.toString(userCreditCard.cvv));
-        paymentType.setCreditCard(creditCard);
-
-        // Create the payment transaction request
-        TransactionRequestType txnRequest = new TransactionRequestType();
-        txnRequest.setTransactionType(TransactionTypeEnum.AUTH_CAPTURE_TRANSACTION.value());
-        txnRequest.setPayment(paymentType);
-        txnRequest.setAmount(new BigDecimal(amount).setScale(2, RoundingMode.CEILING));
-
-        // Make the API Request
-        CreateTransactionRequest apiRequest = new CreateTransactionRequest();
-        apiRequest.setTransactionRequest(txnRequest);
-        CreateTransactionController controller = new CreateTransactionController(apiRequest);
-        controller.execute();
-
-
-        CreateTransactionResponse response = controller.getApiResponse();
-            
-		return response;
+		try {
+	        //Common code to set for all requests
+	        //ApiOperationBase.setEnvironment(Environment.SANDBOX);
+			ApiOperationBase.setEnvironment(Environment.PRODUCTION);
+	
+	        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
+	        merchantAuthenticationType.setName(apiLoginId);
+	        merchantAuthenticationType.setTransactionKey(transactionKey);
+	        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
+	
+	        // Populate the payment data
+	        PaymentType paymentType = new PaymentType();
+	        CreditCardType creditCard = new CreditCardType();
+	        creditCard.setCardNumber(userCreditCard.digits);
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
+	        String expDateStr = userCreditCard.expDate.format(formatter);
+	        creditCard.setExpirationDate(expDateStr);
+	        creditCard.setCardCode(Integer.toString(userCreditCard.cvv));
+	        paymentType.setCreditCard(creditCard);
+	
+	        // Create the payment transaction request
+	        TransactionRequestType txnRequest = new TransactionRequestType();
+	        
+	        txnRequest.setTransactionType(TransactionTypeEnum.AUTH_CAPTURE_TRANSACTION.value());
+	        txnRequest.setPayment(paymentType);
+	        txnRequest.setAmount(new BigDecimal(amount).setScale(2, RoundingMode.CEILING));
+	
+	        // Make the API Request
+	        CreateTransactionRequest apiRequest = new CreateTransactionRequest();
+	        apiRequest.setTransactionRequest(txnRequest);
+	        CreateTransactionController controller = new CreateTransactionController(apiRequest);
+	        controller.execute();
+	
+	
+	        CreateTransactionResponse response = controller.getApiResponse();
+	            
+			return response;
+		}
+		catch (Exception e) {
+			
+			Logger.error(e.getMessage(), e);
+		}
+		return null;
+		
 
     }
 	
